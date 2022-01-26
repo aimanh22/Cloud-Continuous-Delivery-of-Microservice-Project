008ac581326888c9d0c0aafdeedb86b171cdc290
Aiman <69780552+aimanh22@users.noreply.github.com>
2022-01-25 20:36:02 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,5 +1,24 @@
-from app import get_wiki_summary
 from app import getabstract
 
 def test_wiki():
-  assert getabstract("Hello")=
+   url = 'https://en.wikipedia.org/w/api.php'
+    params = {
+            'action': 'parse',
+            'page': "Hello",
+            'format': 'json',
+            'prop':'text',
+            'redirects':''
+            }
+ 
+    response = requests.get(url, params=params)
+    data = response.json()
+ 
+    raw_html = data['parse']['text']['*']
+    soup = BeautifulSoup(raw_html,'html.parser')
+    soup.find_all('p')
+    text = ''
+ 
+    for p in soup.find_all('p'):
+      text += p.text
+      pass
+  assert getabstract("Hello")==text
